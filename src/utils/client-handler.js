87e9fd89b8af61c0c22490e187cdc3623e030d0a
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-07-31 18:54:37 -0400

    Cleanup of attempted AppSync S3Object code.

--- src/utils/client-handler.js
+++ src/utils/client-handler.js
@@ -4,20 +4,12 @@ import { Auth } from 'aws-amplify';
 
 
 export function createClient() {
-    Auth.currentCredentials().then((creds) => console.log('creds:', creds));
-    // console.log('creds:', Auth.currentCredentials());
-    // async () => global.AWS.config.credentials = await Auth.currentCredentials();
     return new AWSAppSyncClient({
         url: aws_config.aws_appsync_graphqlEndpoint,
         region: aws_config.aws_appsync_region,
         auth: {
             type: aws_config.aws_appsync_authenticationType,
             jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
-
-            // TODO: is this necessary?
-            // credentials: () => Auth.currentCredentials(),
         },
-        // TODO: optional for anything without a file
-        complexObjectCredentials: () => Auth.currentCredentials()
     });
 }
