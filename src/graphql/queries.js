5ae8e4344b1935a816adf9e3eebb0314962e2522
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-08-27 17:59:16 -0400

    Update gitignore to amplify autogenerated.
    Prep for pushing to amplify console attempt.

---
+++ src/graphql/queries.js
@@ -0,0 +1,118 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getPublicArtWithinBoundingBox = `query GetPublicArtWithinBoundingBox(
+  $top_right_gps: LocationInput!
+  $bottom_left_gps: LocationInput!
+  $limit: Int
+  $type: String
+) {
+  getPublicArtWithinBoundingBox(
+    top_right_gps: $top_right_gps
+    bottom_left_gps: $bottom_left_gps
+    limit: $limit
+    type: $type
+  ) {
+    id
+    location {
+      lat
+      lon
+    }
+    name
+    description
+    type
+    photos
+  }
+}
+`;
+export const whoAmI = `query WhoAmI {
+  whoAmI
+}
+`;
+export const getPublicArt = `query GetPublicArt($id: ID!) {
+  getPublicArt(id: $id) {
+    id
+    location {
+      lat
+      lon
+    }
+    name
+    description
+    type
+    photos
+  }
+}
+`;
+export const listPublicArts = `query ListPublicArts(
+  $filter: ModelPublicArtFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPublicArts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      type
+      photos
+    }
+    nextToken
+  }
+}
+`;
+export const getReported = `query GetReported($id: ID!) {
+  getReported(id: $id) {
+    id
+    art_id
+    reason
+    reason_continued
+  }
+}
+`;
+export const listReporteds = `query ListReporteds(
+  $filter: ModelReportedFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listReporteds(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      art_id
+      reason
+      reason_continued
+    }
+    nextToken
+  }
+}
+`;
+export const searchPublicArts = `query SearchPublicArts(
+  $filter: SearchablePublicArtFilterInput
+  $sort: SearchablePublicArtSortInput
+  $limit: Int
+  $nextToken: Int
+) {
+  searchPublicArts(
+    filter: $filter
+    sort: $sort
+    limit: $limit
+    nextToken: $nextToken
+  ) {
+    items {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      type
+      photos
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,72 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePublicArt = `subscription OnCreatePublicArt {
+  onCreatePublicArt {
+    id
+    location {
+      lat
+      lon
+    }
+    name
+    description
+    type
+    photos
+  }
+}
+`;
+export const onUpdatePublicArt = `subscription OnUpdatePublicArt {
+  onUpdatePublicArt {
+    id
+    location {
+      lat
+      lon
+    }
+    name
+    description
+    type
+    photos
+  }
+}
+`;
+export const onDeletePublicArt = `subscription OnDeletePublicArt {
+  onDeletePublicArt {
+    id
+    location {
+      lat
+      lon
+    }
+    name
+    description
+    type
+    photos
+  }
+}
+`;
+export const onCreateReported = `subscription OnCreateReported {
+  onCreateReported {
+    id
+    art_id
+    reason
+    reason_continued
+  }
+}
+`;
+export const onUpdateReported = `subscription OnUpdateReported {
+  onUpdateReported {
+    id
+    art_id
+    reason
+    reason_continued
+  }
+}
+`;
+export const onDeleteReported = `subscription OnDeleteReported {
+  onDeleteReported {
+    id
+    art_id
+    reason
+    reason_continued
+  }
+}
+`;
