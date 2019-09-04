9f3111eb0d7956dcafb16474571f9feb2a3fdf0f
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-09-04 13:05:56 -0400

    Update linter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,17 @@ module.exports = {
   },
   'extends': [
     'google',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsdoc/recommended'
   ],
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly',
   },
+  // Needed to correctly lint React.
+  // https://stackoverflow.com/questions/36001552/eslint-parsing-error-unexpected-token
+  'parser': 'babel-eslint',
   'parserOptions': {
     'ecmaFeatures': {
       'jsx': true,
@@ -19,7 +25,35 @@ module.exports = {
   },
   'plugins': [
     'react',
+    'jsdoc'
   ],
   'rules': {
+      'linebreak-style': 0,
+      'valid-jsdoc': 0,
+      'require-jsdoc': 0,
+      'max-len': ['error', {'code': 90, 'ignoreComments': true}]
   },
+  'settings': {
+    'react': {
+      'createClass': 'createReactClass', // Regex for Component Factory to use,
+                                         // default to 'createReactClass'
+      'pragma': 'React',  // Pragma to use, default to 'React'
+      'version': 'detect', // React version. 'detect' automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // default to latest and warns if missing
+                           // It will default to 'detect' in the future
+      'flowVersion': '0.53' // Flow version
+    },
+    'propWrapperFunctions': [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        'forbidExtraProps',
+        {'property': 'freeze', 'object': 'Object'},
+        {'property': 'myFavoriteWrapper'}
+    ],
+    'linkComponents': [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      {'name': 'Link', 'linkAttribute': 'to'}
+    ]
+  }
 };
