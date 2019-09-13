85c666c7a0d9099be452e646d77a12d09395b6c0
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-09-13 08:05:51 -0400

    Browser back button will exit from sidebar, instead of leaving app.

--- src/utils/set-map-and-sidebar-style.js
+++ src/utils/set-map-and-sidebar-style.js
@@ -9,11 +9,21 @@ const largeWindowRatios = {
 const maxSize = '100%';
 const minSize = '0';
 
+let historyPushed = false;
+
 // TODO: center map on clicked location (either new location or a pin)
 /**
  * @param  {boolean} sidebarIsPopulated The sidebar contains information
  */
 export default function setMapAndSidebarStyle(sidebarIsPopulated) {
+  // Pops history state if sidebar already exists.
+  // This allows user to open the sidebar twice without changing the url.
+  // TODO: Hacky, should be redesigned at some point.
+  if (historyPushed) {
+    historyPushed = false;
+    history.back();
+  }
+
   // Detecting screen size with CSS.
   // https://stackoverflow.com/questions/31162606
   const smallWindow = window.matchMedia(
@@ -31,6 +41,21 @@ export default function setMapAndSidebarStyle(sidebarIsPopulated) {
     sidebarDiv.style.width = minSize;
     return;
   }
+
+  // Ensure that we always return to root. Corrects the use of the 'forward' button.
+  // If 'forwarded' into a state with url of /with-sidebar, returns to root instead.
+  history.replaceState(null, null, window.location.origin);
+
+  // Using history api to change effect of back button
+  // Automatically works with sidebar 'close' buttons.
+  // Guide: http://diveintohtml5.info/history.html
+  // API: https://html.spec.whatwg.org/multipage/history.html
+  history.pushState(null, null, window.location.href + 'with-sidebar');
+  historyPushed = true;
+  window.addEventListener('popstate', (event) => {
+    historyPushed = false;
+    setMapAndSidebarStyle(false);
+  });
 
   // For a smaller window, we would like the sidebar to consume the viewport.
   if (smallWindow) {
