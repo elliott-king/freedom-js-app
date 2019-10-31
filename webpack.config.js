d6f6bcc8f3d6a2373ac2c87df421a0311db426f2
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-10-31 19:41:13 -0400

    Programatically add api key to index.html This allows me to use two keys: one for local development, one for the hosted site. I can restrict the keys separately on the Google cloud console.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,56 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+/* eslint-disable */
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.jsx',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+module.exports = env => {
+    return {
+        mode: 'development',
+        entry: './src/app.jsx',
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },{
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
                 }
-            },{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    devServer: {
-        contentBase: './dist',
-        overlay: true,
-        hot: true
-    },
-    plugins: [
-        new CopyWebpackPlugin(['index.html']),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devtool: "source-map",
+            ]
+        },
+        devServer: {
+            contentBase: './dist',
+            overlay: true,
+            hot: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            // Inserting env variables into HTML:
+            // https://medium.com/@trekinbami/using-environment-variables-in-react-6b0a99d83cf5
+            // https://veerasundar.com/blog/2019/01/how-to-inject-environment-values-in-javascript-app-with-webpack/
+            new HtmlWebpackPlugin({
+                filename: 'index.html', 
+                template: 'index.html',
+                apiKey: env.API_KEY,
+            }),
+            new ScriptExtHtmlWebpackPlugin({
+                defaultAttribute: 'defer'
+            })
+        ],
+        devtool: "source-map",
+    };
 };
 
