1f4e319a0f44e978c2b03a33e0beec50fad3e18e
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-01-10 09:40:44 -0500

    Add event type constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,4 +12,26 @@ const additionalOptions = [
   {value: 'all', label: 'Any type'},
 ];
 
-export const ALL_OPTIONS = additionalOptions.concat(SIMPLE_ART_OPTIONS);
+export const EVENT_TYPES = [
+  {value: 'advocacy', label: 'Advocacy'},
+  {value: 'art', label: 'Art'},
+  {value: 'athletics', label: 'Athletics'},
+  {value: 'comedy', label: 'Comedy'},
+  {value: 'dance', label: 'Dance'},
+  {value: 'diy', label: 'DIY'},
+  {value: 'education', label: 'Education'},
+  {value: 'family', label: 'Family'},
+  {value: 'film', label: 'Film'},
+  {value: 'food', label: 'Food'},
+  {value: 'history', label: 'History'},
+  {value: 'holiday', label: 'Holiday'},
+  {value: 'literature', label: 'Literature'},
+  {value: 'lgbtq', label: 'LGBTQ'},
+  {value: 'music', label: 'Music'},
+  {value: 'nature', label: 'Nature'},
+  {value: 'new_york', label: 'New York'},
+  {value: 'philosophy', label: 'Philosophy'},
+  {value: 'science', label: 'Science'},
+  {value: 'shopping', label: 'Shopping'},
+  {value: 'theater', label: 'Theater'},
+];
