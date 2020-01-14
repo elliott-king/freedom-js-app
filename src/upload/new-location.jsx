4996c6477f8bb8f3d774eae35138bd5e7eb7b4ec
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-01-14 12:56:25 -0500

    Add 'event' functionality to location react div

--- src/upload/new-location.jsx
+++ src/upload/new-location.jsx
@@ -6,41 +6,47 @@ import PropTypes from 'prop-types';
 
 import Select from 'react-select';
 import DatePicker from 'react-date-picker';
+import TimePicker from 'react-time-picker';
 import gql from 'graphql-tag';
 import {v4 as uuid} from 'uuid';
 
 import {uploadImage} from './upload-image';
-import {createPublicArt} from '../graphql/mutations';
+import {createPublicArt, createEvent} from '../graphql/mutations';
 import {openSidebar, closeSidebar} from '../utils/set-map-and-sidebar-style';
 import {updateMarkers} from '../utils/markers-utils';
-import {centerMap} from '../utils/map-utils';
-import {SIMPLE_ART_OPTIONS} from '../utils/constants';
+import {SIMPLE_ART_OPTIONS, locationType, EVENT_TYPES} from '../utils/constants';
 
 class PublicArtUploadForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      // shared
       name: '',
       description: '',
-      selectedOption: '',
+      artType: '', // TODO: rename 'artType'
+
+      // public art
       permanent: true,
       start: new Date(),
       end: new Date(),
-      lat: this.props.lat,
-      lon: this.props.lng,
+
+      // event
+      // NOTE: we will ignore the 'source' field for user-supplied content
+      eventTypes: [],
+      host: '',
+      website: '',
+      dates: [new Date()],
+      times: ['12:00'],
+      locationDescription: '',
+      rsvp: false,
+      // TODO: none of the arrays are implemented as multiple select (currently only provide one)
     };
 
     // To upload images in React, we use the file API.
     // https://reactjs.org/docs/uncontrolled-components.html#the-file-input-tag
     this.imageInput = React.createRef();
-
-    this.checkboxChange = this.checkboxChange.bind(this);
-    this.optionChange = this.optionChange.bind(this);
     this.uploadHandler = this.uploadHandler.bind(this);
-    this.nameChange = this.nameChange.bind(this);
-    this.descriptionChange = this.descriptionChange.bind(this);
-    this.userLocation = this.userLocation.bind(this);
   }
 
   componentDidMount() {
@@ -53,45 +59,71 @@ class PublicArtUploadForm extends React.Component {
   uploadHandler(event) {
     event.preventDefault(); // KTHXWHAT?
 
-    if (this.imageInput.current.files.length > 0 &&
-      this.state.name &&
-      this.state.selectedOption) {
-      const locationId = uuid();
-      const imgFile = this.imageInput.current.files[0];
+    if (this.imageInput.current.files.length > 0 && this.state.name) {
+      const hasFieldsPublicArt = Boolean(this.state.artType);
+      const hasFieldsEvent = this.state.eventTypes.length > 0 && this.state.website;
+      if ((this.props.type == locationType.PUBLIC_ART && hasFieldsPublicArt) ||
+        (this.props.type == locationType.EVENT && hasFieldsEvent)) {
+        let gqlMutation = undefined;
+        const locationId = uuid();
+        const imgFile = this.imageInput.current.files[0];
 
-      const input = {
-        id: locationId,
-        name: this.state.name,
-        location: {
-          lat: this.state.lat,
-          lon: this.state.lon,
-        },
-        description: (this.state.description ? this.state.description : undefined),
-        permanent: this.state.permanent,
-        // TODO: we should add a default type.
-        type: this.state.selectedOption.value,
-      };
-
-      if (!this.state.permanent) {
-        input.date_range = {
-          start: this.state.start.toISOString().substr(0, 10),
-          end: this.state.end.toISOString().substr(0, 10),
+        const input = {
+          id: locationId,
+          name: this.state.name,
+          location: {
+            lat: this.props.lat,
+            lon: this.props.lng,
+          },
+          description: (this.state.description ?
+            this.state.description : undefined),
         };
-      }
 
-      window.cognitoClient.mutate({
-        mutation: gql(createPublicArt),
-        variables: {input: input}})
-          .then((createPublicArtResponse) => uploadImage(imgFile, locationId))
-          .then((uploadPhotoResponse) => {
-            console.log('Uploaded new location', this.state.name, 'to dynamodb.');
-            // Finally, remove both the sidebar and the marker.
-            closeSidebar();
-            updateMarkers([]);
-          }).catch((err) => console.log('Cannot create new location:', err));
-    } else if (!this.state.selectedOption) {
-      window.alert('Please choose the type of public art');
-      console.warn('Please choose the type of public art');
+        if (this.props.type == locationType.PUBLIC_ART) {
+          gqlMutation = createPublicArt;
+          input.permanent = this.state.permanent;
+          // TODO: we should add a default type.
+          input.type = this.state.artType;
+
+          if (!this.state.permanent) {
+            input.date_range = {
+              start: this.state.start.toISOString().substr(0, 10),
+              end: this.state.end.toISOString().substr(0, 10),
+            };
+          }
+        } else if (this.props.type == locationType.EVENT) {
+          gqlMutation = createEvent;
+          input.rsvp = this.state.rsvp;
+          input.times = this.state.times;
+          input.dates = this.state.dates.map((d) => d.toISOString().substr(0, 10));
+          input.types = this.state.eventTypes;
+          if (!this.state.website.startsWith('http')) {
+            input.website = 'http://' + this.state.website;
+          } else input.website = this.state.website;
+          if (this.state.host) input.host = this.state.host;
+          if (this.state.locationDescription) {
+            input.location_description = this.state.locationDescription;
+          }
+        } else {
+          // Should never be here, but lets be safe
+          throw new Error('Location does not have a type of \'event\' or \'public art\'');
+        }
+
+        window.cognitoClient.mutate({
+          mutation: gql(gqlMutation), variables: {input: input}})
+            .then((createLocationResponse) => uploadImage(imgFile, locationId))
+            .then((uploadPhotoResponse) => {
+              // Finally, remove both the sidebar and the marker.
+              closeSidebar();
+              updateMarkers([]);
+            }).catch((err) => console.log('Cannot create new location:', err));
+      } else if (!this.state.artType && !(this.state.eventTypes.length > 0)) {
+        window.alert('Please choose one or more tags for the location.');
+        console.warn('Please choose one or more tags for the location.');
+      } else if (this.props.type == locationType.EVENT) {
+        window.alert('Please include a website for this event.');
+        console.warn('Please include a website for this event.');
+      }
     } else if (!this.state.name) {
       window.alert('New location needs a name.');
       console.warn('New location needs a name.');
@@ -101,22 +133,58 @@ class PublicArtUploadForm extends React.Component {
     }
   }
 
-  optionChange(selectedOption) {
-    this.setState({selectedOption: selectedOption});
-  }
-  nameChange(event) {
-    this.setState({name: event.target.value});
-  }
-  checkboxChange(event) {
-    this.setState({permanent: event.target.checked});
+  renderTitle() {
+    if (this.props.type == locationType.PUBLIC_ART) return (<h1>New Public Art</h1>);
+    else return (<h1>New Event</h1>);
   }
 
-  descriptionChange(event) {
-    this.setState({description: event.target.value});
+  renderTypeSelector() {
+    if (this.props.type == locationType.PUBLIC_ART) {
+      return (
+        <Select
+          options={SIMPLE_ART_OPTIONS}
+          onChange={(selectedOption) => this.setState({artType: selectedOption.value})}
+        />
+      );
+    } else if (this.props.type == locationType.EVENT) {
+      return (
+        <Select
+          options={EVENT_TYPES}
+          onChange={
+            (selectedOption) => this.setState({eventTypes: [selectedOption.value]})}
+        />
+      );
+    } else return null;
+  }
+
+  renderCheckbox() {
+    const text = (this.props.type == locationType.PUBLIC_ART ?
+      'Permanent piece' : 'RSVP Necessary');
+    const isChecked = (this.props.type == locationType.PUBLIC_ART ?
+      this.state.permanent : this.state.rsvp);
+    const checkboxChange = (event) => {
+      if (this.props.type == locationType.PUBLIC_ART) {
+        this.setState({permanent: event.target.checked});
+      } else {
+        this.setState({rsvp: event.target.checked});
+      }
+    };
+
+    return (
+      <React.Fragment>
+        <input
+          type="checkbox"
+          label={text}
+          checked={isChecked}
+          onChange={checkboxChange}
+        />
+        <div>{text}</div>
+      </React.Fragment>
+    );
   }
 
   renderDateSelectors() {
-    if (!this.state.permanent) {
+    if (this.props.type == locationType.PUBLIC_ART && !this.state.permanent) {
       return (
         <React.Fragment>
           <DatePicker
@@ -129,85 +197,102 @@ class PublicArtUploadForm extends React.Component {
           />
         </React.Fragment>
       );
+    } else if (this.props.type == locationType.EVENT) {
+      // TODO: time picker looks awful. User should only be able to type
+      return (
+        <React.Fragment>
+          <DatePicker
+            onChange={(val) => this.setState({dates: [val]})}
+            value={this.state.dates[0]}
+          />
+          <TimePicker
+            onChange={(val) => this.setState({times: [val]})}
+            value={this.state.times[0]}
+          />
+        </React.Fragment>
+      );
     } else {
       return null;
     }
   }
 
-  userLocation(event) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=> {
-        this.setState({
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        });
-
-        const newLocationReplacementMarker = new google.maps.Marker({
-          position: {lng: position.coords.longitude, lat: position.coords.latitude},
-          map: window.map,
-          title: 'New location',
-          label: 'N',
-        });
-        updateMarkers([newLocationReplacementMarker]);
-        centerMap();
-      }, function(err) {
-        window.alert('Error: geolocation service has failed:', err);
-        console.warn('Error: geolocation service has failed:', err);
-      });
-    } else {
-      window.alert('Browser does not handle geolocation');
-      console.warn('Browser does not handle geolocation');
-    }
+  renderSimpleEventFields() {
+    if (this.props.type == locationType.EVENT) {
+      return (
+        <React.Fragment>
+          <div className="new-event-website">
+            <p>Website</p>
+            <input className="new-location-input"
+              type="text"
+              placeholder="URL"
+              name="website"
+              value={this.state.website}
+              onChange={(event) => this.setState({website: event.target.value})}
+            />
+          </div>
+          <div className="new-event-host">
+            <p>Host</p>
+            <input className="new-location-input"
+              type="text"
+              placeholder="Host/Venue"
+              name="host"
+              value={this.state.host}
+              onChange={(event) => this.setState({host: event.target.value})}
+            />
+          </div>
+          <div className="new-event-location-description">
+            <p>Address</p>
+            <input className="new-location-input"
+              type="text"
+              placeholder="Address (optional)"
+              name="location-description"
+              value={this.state.locationDescription}
+              onChange={(event) =>
+                this.setState({locationDescription: event.target.value})}
+            />
+          </div>
+        </React.Fragment>
+      );
+    } else return null;
   }
 
   // center div horizontally & vertically
   // https://stackoverflow.com/questions/22658141
   render() {
     return (
-      <div id="new-public-art-upload" className="public-art-upload-form-container">
-        <form onSubmit={this.uploadHandler} className="public-art-upload-form">
-          <h1>New Public Art</h1>
-          <h4 className="new-public-art-location">
-            Location: {this.state.lat.toFixed(2)}, {this.state.lon.toFixed(2)}
+      <div id="new-location-upload" className="location-upload-form-container">
+        <form onSubmit={this.uploadHandler} className="location-upload-form">
+          {this.renderTitle()}
+          <h4 className="new-location-latlng">
+            Location: {this.props.lat.toFixed(3)}, {this.props.lng.toFixed(3)}
           </h4>
-          <button type="button"
-            className="new-public-art-set-location"
-            onClick={this.userLocation}>
-            Use my current location
-          </button>
-          <div className="new-public-art-name">
+          <p>Click the map to change location.</p>
+          <div className="new-location-name">
             <p>Name</p>
-            <input className = "new-public-art-input"
+            <input className = "new-location-input"
               type="text"
               placeholder="Name"
               name="name"
               value={this.state.name}
-              onChange={this.nameChange}
+              onChange={(event) => this.setState({name: event.target.value})}
+              // onChange={this.nameChange}
             />
           </div>
-          <div className="new-public-art-description">
+          <div className="new-location-description">
             <p>Description</p>
-            <input className="new-public-art-input"
+            <input className="new-location-input"
               type="text"
               placeholder="Optional"
               name="description"
               value={this.state.description}
-              onChange={this.descriptionChange}
+              onChange={(event) => this.setState({description: event.target.value})}
             />
           </div>
-          <Select
-            options={SIMPLE_ART_OPTIONS}
-            onChange={this.optionChange}
-          />
-          <input
-            type="checkbox"
-            label="Permanent piece"
-            checked={this.state.permanent}
-            onChange={this.checkboxChange}
-          />
-          <div>Permanent piece</div>
+          {this.renderSimpleEventFields()}
+          {this.renderTypeSelector()}
+          {this.renderCheckbox()}
           {this.renderDateSelectors()}
-          <div className="new-public-art-image">
+          <div className="new-location-image">
             <h4>Choose image for location</h4>
             <input
               type="file"
@@ -224,6 +309,7 @@ class PublicArtUploadForm extends React.Component {
 PublicArtUploadForm.propTypes = {
   lat: PropTypes.number.isRequired,
   lng: PropTypes.number.isRequired,
+  type: PropTypes.number.isRequired,
 };
 
 // TODO: just take in a latLng
