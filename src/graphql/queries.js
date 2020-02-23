0b7b7649211de01dc572355af0cd875d8d3ba851
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-02-22 21:14:05 -0500

    (autog) updates after resetting api key

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,178 +1,114 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getPublicArtWithinBoundingBox = `query GetPublicArtWithinBoundingBox(
-  $search: BoundingBoxInput!
-  $permanent: Boolean!
-  $type: PublicArtType
-) {
-  getPublicArtWithinBoundingBox(
-    search: $search
-    permanent: $permanent
-    type: $type
+export const getPublicArtWithinBoundingBox = /* GraphQL */ `
+  query GetPublicArtWithinBoundingBox(
+    $search: BoundingBoxInput!
+    $permanent: Boolean!
+    $type: PublicArtType
   ) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    type
-    permanent
-    date_range {
-      start
-      end
-    }
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+    getPublicArtWithinBoundingBox(
+      search: $search
+      permanent: $permanent
+      type: $type
+    ) {
+      id
+      location {
+        lat
+        lon
       }
-      nextToken
+      name
+      description
+      type
+      permanent
+      date_range {
+        start
+        end
+      }
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
     }
   }
-}
 `;
-export const getEventWithinBoundingBox = `query GetEventWithinBoundingBox(
-  $search: BoundingBoxInput!
-  $is_public: Boolean!
-  $is_private: Boolean!
-) {
-  getEventWithinBoundingBox(
-    search: $search
-    is_public: $is_public
-    is_private: $is_private
+export const getEventWithinBoundingBox = /* GraphQL */ `
+  query GetEventWithinBoundingBox(
+    $search: BoundingBoxInput!
+    $is_public: Boolean!
+    $is_private: Boolean!
   ) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    types
-    host
-    source
-    website
-    dates
-    times
-    location_description
-    rsvp
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+    getEventWithinBoundingBox(
+      search: $search
+      is_public: $is_public
+      is_private: $is_private
+    ) {
+      id
+      location {
+        lat
+        lon
       }
-      nextToken
+      name
+      description
+      types
+      host
+      source
+      website
+      dates
+      times
+      location_description
+      rsvp
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
     }
   }
-}
 `;
-export const getReported = `query GetReported($id: ID!) {
-  getReported(id: $id) {
-    id
-    art_id
-    reason
-    reason_continued
-  }
-}
-`;
-export const listReporteds = `query ListReporteds(
-  $filter: ModelReportedFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listReporteds(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+export const getReported = /* GraphQL */ `
+  query GetReported($id: ID!) {
+    getReported(id: $id) {
       id
       art_id
       reason
       reason_continued
     }
-    nextToken
   }
-}
 `;
-export const getPublicArt = `query GetPublicArt($id: ID!) {
-  getPublicArt(id: $id) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    type
-    permanent
-    date_range {
-      start
-      end
-    }
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
-      }
-      nextToken
-    }
-  }
-}
-`;
-export const listPublicArts = `query ListPublicArts(
-  $filter: ModelPublicArtFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listPublicArts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      location {
-        lat
-        lon
-      }
-      name
-      description
-      type
-      permanent
-      date_range {
-        start
-        end
-      }
-      owner
-      photos {
-        nextToken
-      }
-    }
-    nextToken
-  }
-}
-`;
-export const searchPublicArts = `query SearchPublicArts(
-  $filter: SearchablePublicArtFilterInput
-  $sort: SearchablePublicArtSortInput
-  $limit: Int
-  $nextToken: String
-) {
-  searchPublicArts(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
+export const listReporteds = /* GraphQL */ `
+  query ListReporteds(
+    $filter: ModelReportedFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
+    listReporteds(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        art_id
+        reason
+        reason_continued
+      }
+      nextToken
+    }
+  }
+`;
+export const getPublicArt = /* GraphQL */ `
+  query GetPublicArt($id: ID!) {
+    getPublicArt(id: $id) {
       id
       location {
         lat
@@ -188,52 +124,88 @@ export const searchPublicArts = `query SearchPublicArts(
       }
       owner
       photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
         nextToken
       }
     }
-    nextToken
-    total
   }
-}
 `;
-export const getEvent = `query GetEvent($id: ID!) {
-  getEvent(id: $id) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    types
-    host
-    source
-    website
-    dates
-    times
-    location_description
-    rsvp
-    owner
-    photos {
+export const listPublicArts = /* GraphQL */ `
+  query ListPublicArts(
+    $filter: ModelPublicArtFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPublicArts(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
-        location_id
-        url
-        user_id
+        location {
+          lat
+          lon
+        }
+        name
+        description
+        type
+        permanent
+        date_range {
+          start
+          end
+        }
         owner
+        photos {
+          nextToken
+        }
       }
       nextToken
     }
   }
-}
 `;
-export const listEvents = `query ListEvents(
-  $filter: ModelEventFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+export const searchPublicArts = /* GraphQL */ `
+  query SearchPublicArts(
+    $filter: SearchablePublicArtFilterInput
+    $sort: SearchablePublicArtSortInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    searchPublicArts(
+      filter: $filter
+      sort: $sort
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        location {
+          lat
+          lon
+        }
+        name
+        description
+        type
+        permanent
+        date_range {
+          start
+          end
+        }
+        owner
+        photos {
+          nextToken
+        }
+      }
+      nextToken
+      total
+    }
+  }
+`;
+export const getEvent = /* GraphQL */ `
+  query GetEvent($id: ID!) {
+    getEvent(id: $id) {
       id
       location {
         lat
@@ -251,75 +223,115 @@ export const listEvents = `query ListEvents(
       rsvp
       owner
       photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
         nextToken
       }
     }
-    nextToken
   }
-}
 `;
-export const searchEvents = `query SearchEvents(
-  $filter: SearchableEventFilterInput
-  $sort: SearchableEventSortInput
-  $limit: Int
-  $nextToken: String
-) {
-  searchEvents(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
+export const listEvents = /* GraphQL */ `
+  query ListEvents(
+    $filter: ModelEventFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      id
-      location {
-        lat
-        lon
+    listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        location {
+          lat
+          lon
+        }
+        name
+        description
+        types
+        host
+        source
+        website
+        dates
+        times
+        location_description
+        rsvp
+        owner
+        photos {
+          nextToken
+        }
       }
-      name
-      description
-      types
-      host
-      source
-      website
-      dates
-      times
-      location_description
-      rsvp
-      owner
-      photos {
-        nextToken
-      }
+      nextToken
     }
-    nextToken
-    total
   }
-}
 `;
-export const getPhoto = `query GetPhoto($id: ID!) {
-  getPhoto(id: $id) {
-    id
-    location_id
-    url
-    user_id
-    owner
+export const searchEvents = /* GraphQL */ `
+  query SearchEvents(
+    $filter: SearchableEventFilterInput
+    $sort: SearchableEventSortInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    searchEvents(
+      filter: $filter
+      sort: $sort
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        location {
+          lat
+          lon
+        }
+        name
+        description
+        types
+        host
+        source
+        website
+        dates
+        times
+        location_description
+        rsvp
+        owner
+        photos {
+          nextToken
+        }
+      }
+      nextToken
+      total
+    }
   }
-}
 `;
-export const listPhotos = `query ListPhotos(
-  $filter: ModelPhotoFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listPhotos(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+export const getPhoto = /* GraphQL */ `
+  query GetPhoto($id: ID!) {
+    getPhoto(id: $id) {
       id
       location_id
       url
       user_id
       owner
     }
-    nextToken
   }
-}
+`;
+export const listPhotos = /* GraphQL */ `
+  query ListPhotos(
+    $filter: ModelPhotoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPhotos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        location_id
+        url
+        user_id
+        owner
+      }
+      nextToken
+    }
+  }
 `;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,240 +1,252 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreatePublicArt = `subscription OnCreatePublicArt($owner: String!) {
-  onCreatePublicArt(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    type
-    permanent
-    date_range {
-      start
-      end
-    }
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+export const onCreatePublicArt = /* GraphQL */ `
+  subscription OnCreatePublicArt($owner: String!) {
+    onCreatePublicArt(owner: $owner) {
+      id
+      location {
+        lat
+        lon
       }
-      nextToken
-    }
-  }
-}
-`;
-export const onUpdatePublicArt = `subscription OnUpdatePublicArt($owner: String!) {
-  onUpdatePublicArt(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    type
-    permanent
-    date_range {
-      start
-      end
-    }
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+      name
+      description
+      type
+      permanent
+      date_range {
+        start
+        end
       }
-      nextToken
-    }
-  }
-}
-`;
-export const onDeletePublicArt = `subscription OnDeletePublicArt($owner: String!) {
-  onDeletePublicArt(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    type
-    permanent
-    date_range {
-      start
-      end
-    }
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
       }
-      nextToken
     }
   }
-}
 `;
-export const onCreateEvent = `subscription OnCreateEvent($owner: String!) {
-  onCreateEvent(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    types
-    host
-    source
-    website
-    dates
-    times
-    location_description
-    rsvp
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+export const onUpdatePublicArt = /* GraphQL */ `
+  subscription OnUpdatePublicArt($owner: String!) {
+    onUpdatePublicArt(owner: $owner) {
+      id
+      location {
+        lat
+        lon
       }
-      nextToken
-    }
-  }
-}
-`;
-export const onUpdateEvent = `subscription OnUpdateEvent($owner: String!) {
-  onUpdateEvent(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    types
-    host
-    source
-    website
-    dates
-    times
-    location_description
-    rsvp
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+      name
+      description
+      type
+      permanent
+      date_range {
+        start
+        end
       }
-      nextToken
-    }
-  }
-}
-`;
-export const onDeleteEvent = `subscription OnDeleteEvent($owner: String!) {
-  onDeleteEvent(owner: $owner) {
-    id
-    location {
-      lat
-      lon
-    }
-    name
-    description
-    types
-    host
-    source
-    website
-    dates
-    times
-    location_description
-    rsvp
-    owner
-    photos {
-      items {
-        id
-        location_id
-        url
-        user_id
-        owner
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
       }
-      nextToken
     }
   }
-}
 `;
-export const onCreateReported = `subscription OnCreateReported {
-  onCreateReported {
-    id
-    art_id
-    reason
-    reason_continued
+export const onDeletePublicArt = /* GraphQL */ `
+  subscription OnDeletePublicArt($owner: String!) {
+    onDeletePublicArt(owner: $owner) {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      type
+      permanent
+      date_range {
+        start
+        end
+      }
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
+    }
   }
-}
 `;
-export const onUpdateReported = `subscription OnUpdateReported {
-  onUpdateReported {
-    id
-    art_id
-    reason
-    reason_continued
+export const onCreateEvent = /* GraphQL */ `
+  subscription OnCreateEvent($owner: String!) {
+    onCreateEvent(owner: $owner) {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      types
+      host
+      source
+      website
+      dates
+      times
+      location_description
+      rsvp
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
+    }
   }
-}
 `;
-export const onDeleteReported = `subscription OnDeleteReported {
-  onDeleteReported {
-    id
-    art_id
-    reason
-    reason_continued
+export const onUpdateEvent = /* GraphQL */ `
+  subscription OnUpdateEvent($owner: String!) {
+    onUpdateEvent(owner: $owner) {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      types
+      host
+      source
+      website
+      dates
+      times
+      location_description
+      rsvp
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
+    }
   }
-}
 `;
-export const onCreatePhoto = `subscription OnCreatePhoto($owner: String!) {
-  onCreatePhoto(owner: $owner) {
-    id
-    location_id
-    url
-    user_id
-    owner
+export const onDeleteEvent = /* GraphQL */ `
+  subscription OnDeleteEvent($owner: String!) {
+    onDeleteEvent(owner: $owner) {
+      id
+      location {
+        lat
+        lon
+      }
+      name
+      description
+      types
+      host
+      source
+      website
+      dates
+      times
+      location_description
+      rsvp
+      owner
+      photos {
+        items {
+          id
+          location_id
+          url
+          user_id
+          owner
+        }
+        nextToken
+      }
+    }
   }
-}
 `;
-export const onUpdatePhoto = `subscription OnUpdatePhoto($owner: String!) {
-  onUpdatePhoto(owner: $owner) {
-    id
-    location_id
-    url
-    user_id
-    owner
+export const onCreateReported = /* GraphQL */ `
+  subscription OnCreateReported {
+    onCreateReported {
+      id
+      art_id
+      reason
+      reason_continued
+    }
   }
-}
 `;
-export const onDeletePhoto = `subscription OnDeletePhoto($owner: String!) {
-  onDeletePhoto(owner: $owner) {
-    id
-    location_id
-    url
-    user_id
-    owner
+export const onUpdateReported = /* GraphQL */ `
+  subscription OnUpdateReported {
+    onUpdateReported {
+      id
+      art_id
+      reason
+      reason_continued
+    }
   }
-}
+`;
+export const onDeleteReported = /* GraphQL */ `
+  subscription OnDeleteReported {
+    onDeleteReported {
+      id
+      art_id
+      reason
+      reason_continued
+    }
+  }
+`;
+export const onCreatePhoto = /* GraphQL */ `
+  subscription OnCreatePhoto($owner: String!) {
+    onCreatePhoto(owner: $owner) {
+      id
+      location_id
+      url
+      user_id
+      owner
+    }
+  }
+`;
+export const onUpdatePhoto = /* GraphQL */ `
+  subscription OnUpdatePhoto($owner: String!) {
+    onUpdatePhoto(owner: $owner) {
+      id
+      location_id
+      url
+      user_id
+      owner
+    }
+  }
+`;
+export const onDeletePhoto = /* GraphQL */ `
+  subscription OnDeletePhoto($owner: String!) {
+    onDeletePhoto(owner: $owner) {
+      id
+      location_id
+      url
+      user_id
+      owner
+    }
+  }
 `;
