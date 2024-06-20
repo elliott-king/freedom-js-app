a09628db7c3113ede9c0e4164ffbf101b33d7392
elliott-king <8884571+elliott-king@users.noreply.github.com>
2024-06-20 07:50:37 -0400

    Lint legacy app

--- legacy/src/app.jsx
+++ legacy/src/app.jsx
@@ -1,20 +1,20 @@
-import '../css/style.css';
+import "../css/style.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Amplify from 'aws-amplify';
+import Amplify from "aws-amplify";
 // eslint-disable-next-line camelcase
-import aws_config from './aws-exports';
+import aws_config from "./aws-exports";
 
-import {createClient} from './utils/aws-client-handler';
-import Root from './root.jsx';
+import { createClient } from "./utils/aws-client-handler";
+import Root from "./root.jsx";
 
 // Required setup for AWS Amplify utilities (API, S3, Auth, etc..)
 Amplify.configure(aws_config);
 
 // We create global auth clients so anything can call gql queries
-console.log('creating client...');
+console.log("creating client...");
 createClient();
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
